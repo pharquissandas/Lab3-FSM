d800214ee42f62a4e4b3df27bca2cf14c871f413
Preet Harquissandas <preetharquissandas@gmail.com>
2025-10-30 19:08:57 +0000

    Working task 2

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -5,4 +5,12 @@ module lfsr(
     output  logic [3:0] data_out
 );
 
+always_ff @(posedge clk, posedge rst) begin
+    if (rst)
+        data_out <= 4'b1;
+    else if (en) begin
+        data_out <= {data_out[2:0], data_out[3] ^ data_out[2]};
+    end
+end
 endmodule
+
--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -5,4 +5,10 @@ module lfsr_7 (
     output  logic [6:0] data_out
 );
 
+always_ff @(posedge clk, posedge rst) begin
+    if (rst)
+        data_out <= 7'b1;
+    else if (en)
+        data_out <= {data_out[5:0], data_out[6] ^ data_out[2]};
+end
 endmodule
